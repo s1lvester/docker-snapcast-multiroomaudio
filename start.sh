42dd7ae594db56bbcdb9af019acb1554376477e3
Dave Snigier <dave@davesnigier.com>
2017-11-04 19:53:20 -0400

    fix error on startup

--- start.sh
+++ start.sh
@@ -4,8 +4,7 @@ export SPOTIFY_NAME="${SPOTIFY_NAME:=Snapcast}"
 export SPOTIFY_BITRATE="${SPOTIFY_BITRATE:=160}"
 
 # setup dbus
-rm -rf /var/run/dbus
-mkdir -p /var/run/dbus
+rm -rf /var/run/dbus || mkdir -p /var/run/dbus
 chown -R messagebus:messagebus /var/run/dbus
 dbus-uuidgen --ensure
 dbus-daemon --system --fork
